97fba1f07d2b198ea9a74007f91aee3115db9588
Masato Kiryu <kiri9120@gmail.com>
2021-11-14 14:04:54 +0900

    create navbar

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 $(function(){
+    var headerHeight = 70;
     $('a[href^="#"]').click(function(){
       var speed = 500;
       var href= $(this).attr("href");
       var target = $(href == "#" || href == "" ? 'html' : href);
-      var position = target.offset().top;
+      var position = target.offset().top - headerHeight;
       $("html, body").animate({scrollTop:position}, speed, "swing");
       return false;
     });
+});
+
+
+
+$(function() {
+  $('.btn-gNav').on("click", function(){
+
+    $(this).toggleClass('open');
+    $('#gNav').toggleClass('open');
+  });
+
+});
+
+// メニューをクリックされたら、非表示にする
+$(function() {
+  $('.gNav-menu li a').on("click", function(){
+    $('.btn-gNav').removeClass('open');
+    $('#gNav').removeClass('open');
+  });
 });
